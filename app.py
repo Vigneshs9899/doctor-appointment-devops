266bb9c68042358594c85724d9dbf7887f6aeb5d
Vigneshs9899 <vigneshs9899@gmail.com>
2026-07-03 15:36:19 +0530

    add env variables

--- app.py
+++ app.py
@@ -52,10 +52,11 @@ def dashboard():
 while True:
     try:
         conn = psycopg2.connect(
-            host="db",
-            database="appointments_db",
-            user="admin",
-            password="admin123"
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT", "5432")
         )
 
         cursor = conn.cursor()
